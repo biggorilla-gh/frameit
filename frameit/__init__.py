4e03763886fa371230c91106433af197e373e117
Jonathan Yehuda Engel <jonathan@recruit.ai>
2018-12-17 10:44:25 -0800

    adding imports

--- frameit/__init__.py
+++ frameit/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for FrameIt."""
+from frameit.corpus import Corpus
+from frameit.utterance import Utterance
+from frameit.frameattr import FrameAttribute
+from frameit.text_processing import TextProcessing
+from frameit.frame import Frame
+from frameit.srl import SRL, FrameLabel
+
 
 __author__ = """BigGorilla"""
 __email__ = 'thebiggorilla.team@gmail.com'
